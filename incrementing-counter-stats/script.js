c45d497003de988a7b1a6b6e17a95514d81ac4a2
Rx-devs <rxdevs@gmail.com>
2023-01-11 20:08:11 +0600

    incrementing counter stats added

---
+++ incrementing-counter-stats/script.js
@@ -0,0 +1,30 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  // set the counter to zero
+  counter.innerText = "0";
+
+  // target in data-target attribute and take its value
+  const target = +counter.getAttribute("data-target");
+  // console.log(typeof target, target);
+
+  // now update the counter with this function
+  const updateCounter = () => {
+    // set the counter value to here.
+    const prevCounter = +counter.innerText;
+
+    // the incrementor
+    const increment = target / 10;
+
+    if (prevCounter < target) {
+      //  if target is bigger than the prevCounter, counter will be updated
+      counter.innerText = `${Math.ceil(prevCounter + increment)}`;
+      // call updateCounter function after specific times with setTimeout function
+      setTimeout(updateCounter, 100);
+    } else {
+      // otherwise change the counter as targeted value
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
